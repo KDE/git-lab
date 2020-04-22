2538c4c00664b49c830303913a1c83b1ffd5190a
Jonah Brüchert <jbb.prv@gmx.de>
2020-04-22 14:55:05 +0200

    Implement git-lab feature

--- lab/__init__.py
+++ lab/__init__.py
@@ -9,11 +9,16 @@ Base module for the lab package
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import argparse
+import os
 
 from lab.mergerequestcreator import MergeRequestCreator
 from lab.mergerequestcheckout import MergeRequestCheckout
 from lab.mergerequestlist import MergeRequestList
 from lab.config import Config
+from lab.utils import Utils, LogType
+
+from git import Repo
+from git.exc import GitCommandError
 
 
 def main() -> None:
@@ -28,6 +33,7 @@ def main() -> None:
     parser_patch = subparsers.add_parser("patch", help="check out a remote merge request")
     parser_login = subparsers.add_parser("login", help="Save a token for a GitLab token")
     parser_list = subparsers.add_parser("list", help="List open merge requests")
+    parser_feature = subparsers.add_parser("feature", help="Create branches and list branches")
 
     parser_diff.add_argument(
         "--target-branch", help="Use different target branch than master", default="master"
@@ -55,6 +61,14 @@ def main() -> None:
         "--closed", help="Show closed merge requests", action="store_true",
     )
 
+    parser_feature.add_argument(
+        "name", nargs="?", help="name for the new branch"
+    )
+
+    parser_feature.add_argument(
+        "start", nargs="?", help="starting point for the new branch", default="HEAD"
+    )
+
     args: argparse.Namespace = parser.parse_args()
     if args.subcommand == "diff":
         creator: MergeRequestCreator = MergeRequestCreator(args.target_branch)
@@ -71,6 +85,23 @@ def main() -> None:
     elif args.subcommand == "list":
         lister = MergeRequestList(args.project, args.merged, args.opened, args.closed)
         lister.print_formatted_list()
+    elif args.subcommand == "feature":
+        repo = Repo(os.getcwd())
+        git = repo.git
+
+        if args.name:
+            try:
+                if args.name in repo.refs:
+                    git.checkout(args.name)
+                    Utils.log(LogType.Info, "Switched to branch '{}'".format(args.name))
+                else:
+                    git.checkout(args.start, b=args.name) # create a new branch
+                    Utils.log(LogType.Info, "Switched to a new branch '{}'".format(args.name))
+
+            except GitCommandError as e:
+                Utils.log(LogType.Error, e.stderr.strip())
+        else:
+            print(git.branch())
     else:
         parser.print_help()
 
